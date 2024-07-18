d1d100805e10ddab3c565335fa63762ae8ea5947
Himanshu Hembram <himanshu456ia@gmail.com>
2024-07-18 17:33:49 +0530

    initial

---
+++ EJS/Passing+Data/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+   res.render("index.ejs")
+});
+
+app.post("/submit", (req, res) => {
+    const numLel = req.body["fName"].length + req.body["lName"].length;
+    console.log( req.body["fName"] );
+    console.log(numLel);
+
+    res.render("index.ejs", {letters: numLel });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
